7799b86a231db24bd4c97cad1bdd10122fdf0ec1
adha setiawan <adhasetiawanwiyana@gmail.com>
2021-05-31 20:44:00 +0800

    just a little bit more

--- app/src/main/java/com/example/devicelocation/MainActivity.kt
+++ app/src/main/java/com/example/devicelocation/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.example.devicelocation
 
-import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,43 +15,78 @@ class MainActivity : AppCompatActivity() {
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
 
+    private var lat : Double = 0.0
+    private var lng : Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        autolocate()
+        relocate()
+        selectionact()
+    }
+
+    private fun autolocate(){
         //permission
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1000)
-        }else{
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1000
+            )
+        } else {
             buildLocationRequest()
             buildLocationCallback()
 
             fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
             //trigger
-            btn_locate.setOnClickListener {
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    &&
-                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                    {
-                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), 1000)
-                }
-                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                &&
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    1000
+                )
             }
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    private fun relocate(){
+        btn_locate.setOnClickListener {
+            autolocate()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            1000 ->{
-                if (grantResults.size > 0){
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.size > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                         Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-                    }else{
+                    } else {
                         Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
                     }
                 }
@@ -67,12 +95,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buildLocationCallback() {
-        locationCallback = object : LocationCallback(){
+        locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
                 super.onLocationResult(p0)
-                var location = p0.locations.get(p0.locations.size-1)
-                txt_lat.text = location.latitude.toString()
-                txt_long.text = location.longitude.toString()
+                val location = p0.locations.get(p0.locations.size - 1)
+                lat = location.latitude
+                lng = location.longitude
+
+                txt_lat.text = lat.toString()
+                txt_long.text = lng.toString()
             }
         }
     }
@@ -83,6 +114,12 @@ class MainActivity : AppCompatActivity() {
         locationRequest.interval = 5000
         locationRequest.fastestInterval = 3000
         locationRequest.smallestDisplacement = 10f
+    }
+
+    private fun selectionact(){
+        btn_toast.setOnClickListener {
+            Toast.makeText(this, "lat : " + lat, Toast.LENGTH_SHORT).show()
+        }
     }
 
 //    private fun distanceCalcualte(){
